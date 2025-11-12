3adba865b88bf7af687414d031ed84e99c192d97
Topseen-a <abodunrintopseen@gmail.com>
2025-11-12 20:40:56 +0100

    first commit

---
+++ DeclareVariables.java
@@ -0,0 +1,9 @@
+public class DeclareVariables {
+    public static void main (String[] args){
+    
+        int c = 0;
+        int thisIsAVariable = 0;
+        int q76354 = 0;
+        int number = 0;
+    }
+}    
---
+++ VariableNumber.java
@@ -0,0 +1,9 @@
+    public class VariableNumber{
+        public static void main(String[]args){
+        int variableNumber = 5;
+
+      if (variableNumber != 7){
+        System.out.println("The variable number is not equal to 7");        
+        }    
+    }
+}
