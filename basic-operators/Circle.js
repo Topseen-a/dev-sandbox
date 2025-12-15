eb2c1672435285a2807c704398af9d614e11ef80
Topseen-a <abodunrintopseen@gmail.com>
2025-12-15 16:59:55 +0100

    sandbox practice

---
+++ basic-operators/Circle.js
@@ -0,0 +1,8 @@
+let radius = 2;
+let diameter = 2 * radius;
+let pie = 3.14159;
+let circumference = 2;
+console.log('Diameter is', diameter);
+console.log('Circumference is', + pie + radius);
+console.log('Area is', + pie + radius * radius);
+
